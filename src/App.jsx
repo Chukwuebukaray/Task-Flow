cd9d5db4102c69636e8f67f8a87be9a801349509
Chukwuebukaray <chukwuebukaray@gmail.com>
2024-10-04 21:48:01 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./Sidebar/Sidebar.jsx";
 import RightMenu from "./RightMenu/RightMenu.jsx";
 import "./App.css";
